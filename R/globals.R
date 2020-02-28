a44db8587e15fdd6fc05870dc0595af6cb994cb6
fdzul <felipe.dzul.m@gmail.com>
2020-02-28 11:49:59 -0600

    This commit add the global variables

---
+++ R/globals.R
@@ -0,0 +1,3 @@
+utils::globalVariables(c("Localidad", "Semana.Epidemiologica", "read.table",
+                         "Huevecillos", "data", "NOMBRE", "Pocision_Y",
+                         "Pocision_Y", "plot", "mesh", "pred_mean"))
