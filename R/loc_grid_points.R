bcdb247324431514910d61609c3a65180dd83f55
fdzul <felipe.dzul.m@gmail.com>
2020-03-27 10:23:39 -0600

    This commit eliminate the printing a plot

--- R/loc_grid_points.R
+++ R/loc_grid_points.R
@@ -1,6 +1,6 @@
 #' loc_grid_points
 #'
-#' this function make the prediction gris for spde modelling
+#' this function make the prediction grid for spde modelling
 #'
 #' @param sf the sf locality.
 #' @param cell_size  number of cell.
@@ -15,7 +15,7 @@ loc_grid_points <- function(sf, cell_size){
     x <- seq(bb$xmin -1, bb$xmax + 1, length.out = cell_size)
     y <- seq(bb$ymin -1, bb$ymax + 1, length.out = cell_size)
     p <- as.matrix(expand.grid(x, y))
-    plot(p, asp = 1)
+    #plot(p, asp = 1)
     p <- sf::st_as_sf(data.frame(x = p[,1], y = p[,2]),
                       coords = c("x", "y"),
                       crs = 4326)
