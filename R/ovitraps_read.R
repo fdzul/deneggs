b4e5c1d562c95e81576ec3a9b94a448e9ac1b7cd
Felipe Dzul <felipe.dzul.m@gmail.com>
2020-11-23 14:19:00 -0600

    This commit is for document the function and ad the detail in the function.

--- R/ovitraps_read.R
+++ R/ovitraps_read.R
@@ -12,6 +12,11 @@
 #' @export
 #'
 #' @examples 1+1
+#' @references xxxxx
+#'
+#' @seealso \link[boldenr]{read_dataset_bol}
+#'
+#' @details the current and historic ovitrap dataset is download of the homepage of \href{**Sistema Integral de Monitoreo de Vectores**}{https://www.inegi.org.mx/default.html}.
 ovitraps_read <- function(path, current_year, year = NULL){
 
     ## Step 0.1 load the ovitrap dataset ####
