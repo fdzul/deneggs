a663bc4da24dc5829079428781e5f0e71cb436a2
Felipe Dzul <felipe.dzul.m@gmail.com>
2020-11-23 14:09:34 -0600

    This commit document the ovitraps_read function

--- R/globals.R
+++ R/globals.R
@@ -2,4 +2,6 @@ utils::globalVariables(c("Localidad", "Semana.Epidemiologica", "read.table",
                          "Huevecillos", "data", "NOMBRE", "Pocision_Y",
                          "Pocision_X", "plot", "mesh", "pred_mean", "points",
                          "z_score", "hotspots_eggs", "getis_ord_umbral", "getis_ord_umbral",
-                         "getis_ord", "voronoipolygons", "NOMGEO", "long", "AMBITO"))
+                         "getis_ord", "voronoipolygons", "NOMGEO", "long", "AMBITO",
+                         "Clave", "Ovitramps", "Fecha.Lectura",
+                         "unzip_read"))
