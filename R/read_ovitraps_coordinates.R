442bf39af7855f638d2bac5577328db855e381cb
fdzul <felipe.dzul.m@gmail.com>
2024-02-17 13:24:30 -0600

    This commit update the function

--- R/read_ovitraps_coordinates.R
+++ R/read_ovitraps_coordinates.R
@@ -49,7 +49,7 @@ read_ovitraps_coordinates <- function(path_ovitraps,
         dplyr::filter(!is.na(long))  |>
         dplyr::filter(!is.na(eggs))  |>
         dplyr::mutate(Clave = NULL,
-                      Ovitrampa = NULL)
+                      Ovitrampa = NULL) |>
         sf::st_as_sf(coords = c("long", "lat"),
                      crs = 4326)
 
