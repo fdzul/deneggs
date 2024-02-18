4fd4354b6bddf0f831bd517426f277923605ebc6
fdzul <felipe.dzul.m@gmail.com>
2024-02-18 08:30:33 -0600

    this commit update the mesh function

--- R/mesh.R
+++ R/mesh.R
@@ -18,26 +18,26 @@
 #' @examples 1+1
 mesh <- function(x, k, long, lat, loc_limit, plot = NULL){
     coor <- cbind(x[,c(long)], x[, c(lat)])
-    #mesh <- INLA::inla.mesh.2d(coor, ## provide locations or domain #
-    #                           boundary = loc_limit,
-    #                           max.edge = c(0.3/k, 2/k), ## mandatory
-    #                           cutoff= 0.1/k,
-    #                           crs = "+proj=longlat +datum=WGS84") ## good to have >0
+    mesh <- INLA::inla.mesh.2d(coor, ## provide locations or domain #
+                               boundary = loc_limit,
+                               max.edge = c(0.3/k, 2/k), ## mandatory
+                               cutoff= 0.1/k,
+                               crs = fmesher::fm_CRS("longlat_globe")) ## good to have >0
 
-    mesh <- fmesher::fm_mesh_2d_inla(loc = coor,
-                                     boundary = loc_limit,
-                                     max.edge = c(0.3/k, 2/k), ## mandatory
-                                     cutoff= 0.1/k,
-                                     crs = fmesher::fm_CRS("longlat_globe"))
+    #mesh <- fmesher::fm_mesh_2d_inla(loc = coor,
+    #                                 boundary = loc_limit,
+    #                                 max.edge = c(0.3/k, 2/k), ## mandatory
+    #                                 cutoff= 0.1/k,
+    #                                 crs = fmesher::fm_CRS("longlat_globe"))
 
     if(plot == TRUE){
-        #plot(mesh, asp=1, main = ""); points(coor, col= "red")
-        ggplot2::ggplot() +
-            inlabru::gg(mesh) +
-            ggplot2::geom_point(data = as.data.frame(coor),
-                                ggplot2::aes(x = Pocision_X,
-                                             y = Pocision_Y),
-                                col = "red")
+        plot(mesh, asp=1, main = ""); points(coor, col= "red")
+        #ggplot2::ggplot() +
+        #    inlabru::gg(mesh) +
+        #    ggplot2::geom_point(data = as.data.frame(coor),
+        #                        ggplot2::aes(x = Pocision_X,
+        #                                     y = Pocision_Y),
+        #                        col = "red")
     } else {
 
     }
