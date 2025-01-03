84d5ae5e419f2b5a0f7c37f5f8c04a39b5453a32
fdzul <felipe.dzul.m@gmail.com>
2025-01-02 18:58:50 -0600

    this commit update the deneggs pkg

--- R/eggs_hotspots.R
+++ R/eggs_hotspots.R
@@ -51,7 +51,8 @@ eggs_hotspots <- function(path_lect, year = NULL, locality, path_coord,cve_ent,
                                     current_year = FALSE,
                                     year = c(year))  |>
             dplyr::arrange(week)  |>
-            dplyr::filter(week %in% c(sem))
+            dplyr::filter(week %in% c(sem)) |>
+            dplyr::mutate(ovitrap = as.numeric(ovitrap))
     }
 
 
@@ -88,6 +89,7 @@ eggs_hotspots <- function(path_lect, year = NULL, locality, path_coord,cve_ent,
     # Step 0.3 joint the coordinates ####
     #x$ovitrap <- as.numeric(x$ovitrap)
     #y$clave <- as.numeric(y$clave)
+
 
     x <- dplyr::left_join(x = x,
                           y = y,
