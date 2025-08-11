81c1eba8fb2989a7f74aea2fd000168f4930a237
Felipe Dzul <fdzul@MacBook-Pro-de-Felipe-2.local>
2025-08-11 14:07:49 -0600

    This commit update the dash

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' Polygons of blocks from Mexico of the National Electoral Institute 2020
+#'
+#' A spatial dataset containing of polygon data of blocks from Mexico
+#'
+#' @format A sf object with 2278337 rows and 19 variables:
+#' \describe{
+#'   \item{clave}{the ids of ovitrap.}
+#'   \item{entidad}{key and state name.}
+#'   \item{jurisdiccion}{key and jurisdiction name.}
+#'   \item{municipio}{key and munucipality name.}
+#'   \item{localidad}{key and locality name.}
+#'   \item{sector}{key to the sector.}
+#'   \item{manzana}{key of blocks.}
+#'   \item{ovitrampa}{key of blocks.}
+#'   \item{tipo_de_escuela}{type of school.}
+#'   \item{clave_sep}{key of sep.}
+#'   \item{posision_x}{longitude.}
+#'   \item{posision_y}{latitude}
+#'   \item{semana_epidemiologica}{epidemiological week.}
+#'   \item{usuario}{user key.}
+#'   \item{fecha_captura}{capture date}
+#'   ...
+#' }
+#' @details coordinates of ovitraps 2015-08-11.
+"coords"
