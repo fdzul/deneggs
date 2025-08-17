108f7c72103122635243585ee0de0bd303647774
fdzul <felipe.dzul.m@gmail.com>
2025-08-17 11:04:52 -0600

    this commit update the dash

--- R/animap_vector_hotblocks.R
+++ R/animap_vector_hotblocks.R
@@ -1,8 +1,9 @@
 #' animap_vector_hotblocks: a function for create the animated map of hotblocks of eggs.
 #'
-#' @param path_vector is the directory of ovitrap datasets.
+#' @param path_ovitraps is the directory of ovitrap datasets.
 #' @param cve_edo is the text id of the state.
 #' @param locality is the locality target with the ovitraps.
+#' @param year is the year target.
 #' @param dir is the directory where the animation will be saved.
 #' @param name is the name of the gif file.
 #' @param vel is the delay time between images. See also \link[tmap]{tmap_animation}.
@@ -15,42 +16,28 @@
 #' @seealso \link[tmap]{tmap_animation}
 #'
 #' @examples 1+1
-animap_vector_hotblocks <- function(path_vector, locality,cve_edo,
+animap_vector_hotblocks <- function(path_ovitraps, locality,cve_edo,
                                     dir, name, vel, xleg, yleg){
     # Step 1. load the dataset ####
-    x  <- boldenr::read_dataset_bol(path = path_vector,
-                                    dataset = "vectores",
-                                    inf = "Lecturas") |>
-        dplyr::filter(Localidad %in% c(similiars::find_most_similar_string(locality, unique(Localidad)))) |>
-        dplyr::group_by(Semana.Epidemiologica, Localidad, Sector, Manzana) |>
-        dplyr::summarise(mean = round(mean(Huevecillos, na.rm = TRUE), digits = 1)) |>
-        dplyr::mutate(sec_manz = paste(Sector, Manzana, sep = "")) |>
-        tidyr::pivot_wider(id_cols = c( Localidad, sec_manz),
-                           names_from = Semana.Epidemiologica,
-                           values_from = mean)
+    x <- deneggs::ovitraps_read(path = path_ovitraps,
+                                current_year = TRUE) |>
+        dplyr::filter(Localidad %in% c(rgeomex::find_most_similar_string(locality,
+                                                                         unique(Localidad)))) |>
+        dplyr::group_by(year, week, Localidad, sector, manzana) |>
+        dplyr::summarise(mean = round(mean(eggs, na.rm = TRUE), digits = 1),
+                         .groups = "drop") |>
+        dplyr::mutate(sec_manz = paste(sector, manzana, sep = "")) |>
+        tidyr::pivot_wider(id_cols = c(year, Localidad, sec_manz),
+                           names_from = week,
+                           values_from = mean) |>
+        dplyr::filter(year %in% c(year))
 
     # Step 2. load the blocks ####
-    if(cve_edo %in% c(1:10)){
-        y <- rgeomex::blocks_ine20_mx_a |>
-            sf::st_make_valid() |>
-            dplyr::mutate(sec_manz =  paste(seccion, manzana, sep = ""))}
-    if(cve_edo %in% c(11:14)){y <- rgeomex::blocks_ine20_mx_b |>
-        sf::st_make_valid() |>
-        dplyr::mutate(sec_manz =  paste(seccion, manzana, sep = ""))}
-    if(cve_edo %in% c(15:19)){y <- rgeomex::blocks_ine20_mx_c |>
-        sf::st_make_valid() |>
-        dplyr::mutate(sec_manz =  paste(seccion, manzana, sep = ""))}
-    if(cve_edo %in% c(20:25)){y <- rgeomex::blocks_ine20_mx_d |>
-        sf::st_make_valid() |>
-        dplyr::mutate(sec_manz =  paste(seccion, manzana, sep = ""))}
-    if(cve_edo %in% c(26:32)){y <- rgeomex::blocks_ine20_mx_e |>
-        sf::st_make_valid() |>
-        dplyr::mutate(sec_manz =  paste(seccion, manzana, sep = ""))}
+    y <- rgeomex::extract_blocks(cve_edo = cve_edo,
+                                 locality = locality)
 
     # Step 3. load the locality shapefile ####
-    z <- rgeomex::loc_inegi19_mx |>
-        dplyr::filter(NOMGEO %in% c(similiars::find_most_similar_string(locality, unique(NOMGEO))) &
-                          CVE_ENT %in% c(cve_edo))
+    z <- y$locality
 
     if(nrow(z) > 1){
         z <- z |>   sf::st_union()
@@ -59,10 +46,19 @@ animap_vector_hotblocks <- function(path_vector, locality,cve_edo,
     }
 
     # Step 4. Extract the blocks of the locality ####
-    y <- y[z,]
+    blocks <- y$block |>
+        dplyr::mutate(seccion = stringr::str_pad(seccion,
+                                                 width = 4,
+                                                 side = "left",
+                                                 pad = "0"),
+                      manzana = stringr::str_pad(manzana,
+                                                 width = 4,
+                                                 side = "left",
+                                                 pad = "0")) |>
+        dplyr::mutate(sec_manz = paste0(seccion, manzana))
 
     # Step 5. joint the dataset ####
-    xy <- dplyr::left_join(x = y,
+    xy <- dplyr::left_join(x = blocks,
                            y = x,
                            by = c("sec_manz")) |>
         dplyr::filter(!is.na(Localidad)) |>
@@ -82,7 +78,7 @@ animap_vector_hotblocks <- function(path_vector, locality,cve_edo,
         tidyr::unnest(cols = c(risk)) |>
         as.data.frame() |>
         dplyr::mutate(week2 = forcats::fct_reorder(paste("Semana",
-                                                         week, sep = " "),
+                                                         week, year, sep = " "),
                                                    week)) |>
         sf::st_set_geometry(value = "geometry")
 
@@ -94,16 +90,18 @@ animap_vector_hotblocks <- function(path_vector, locality,cve_edo,
                           border.col = "white",
                           lwd = 0.01) +
         tmap::tm_shape(shp = xy |> dplyr::filter(!is.na(risk))) +
-        tmap::tm_fill(col = "risk",
-                      style = "cat",
-                      title = "",
-                      palette = c("#F44B1FFF",
-                                  "#FF9000FF",
-                                  "#35BFFFFF",
-                                  "#00F293FF")) +
+        tmap::tm_polygons(fill = "risk",
+                          fill.scale = tmap::tm_scale_categorical(),
+                          fill.legend = tmap::tm_legend("risk"),
+                          palette = c("#F44B1FFF",
+                                      "#FF9000FF",
+                                      "#35BFFFFF",
+                                      "#00F293FF")) +
         tmap::tm_borders(col = "white", lwd = .5) +
-        tmap::tm_facets(along = "week2", free.coords = FALSE) +
+        tmap::tm_facets(pages = "week2", free.coords = FALSE) +
         tmap::tm_layout(legend.position = c(xleg, yleg))
+
+
     tmap::tmap_animation(animated_map,
                          dpi = 300,
                          delay = vel,
